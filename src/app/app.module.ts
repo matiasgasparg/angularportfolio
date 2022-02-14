5b66f7c446338ae3e8d07e1c962d0242786cf5ee
matiasgasparg <matiasgasparg@gmail.com>
2022-02-14 04:20:17 -0300

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { NavMenuSocialmediaComponent } from './components/nav-menu-socialmedia/nav-menu-socialmedia.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    NavMenuSocialmediaComponent,
+    HeaderComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/nav-menu-socialmedia/nav-menu-socialmedia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu-socialmedia',
+  templateUrl: './nav-menu-socialmedia.component.html',
+  styleUrls: ['./nav-menu-socialmedia.component.css']
+})
+export class NavMenuSocialmediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
